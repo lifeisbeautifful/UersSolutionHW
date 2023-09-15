715e4f09258cfc4bd1c997be4145eb4b7b0546c4
ognyp <Oksankagnyp@gmail.com>
2023-09-15 11:33:33 +0300

    Created user class and added properties

---
+++ UersSolutionHW/User.cs
@@ -0,0 +1,16 @@
+﻿namespace UersSolutionHW
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class User
+    {
+        public string FirstName { get; init; }
+        public string Lastname { get; init; }
+        public int Age { get; init; }
+        public Gender UserGender { get; set; }
+    }
+}
